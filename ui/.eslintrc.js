3f051d554c86244a587fad633c4fe3bb8a4d107f
Claudio Goffi <claudio.goffi@cscs.ch>
2025-10-31 15:36:25 +0100

    update eslint-plugin-jest

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    // 'plugin:jest/recommended',
+    'plugin:jest/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'prettier'
